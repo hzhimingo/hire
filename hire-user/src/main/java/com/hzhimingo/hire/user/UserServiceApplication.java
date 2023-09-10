05f13d61846013543ba5207d2e82d26f5858e981
zhimingo <huangzhimingme@foxmail.com>
2023-09-10 21:15:01 +0800

    引入SpringCloud & SpringCloudAlibaba

--- hire-user/src/main/java/com/hzhimingo/hire/user/UserServiceApplication.java
+++ hire-user/src/main/java/com/hzhimingo/hire/user/UserServiceApplication.java
@@ -3,8 +3,10 @@ package com.hzhimingo.hire.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan(basePackages = "com.hzhimingo.hire.user.mapper")
 public class UserServiceApplication {
 
